69de49a81e7ddf1196560fa896cff78b36775cc0
ValterGames-Coder <68495491+ValterGames-Coder@users.noreply.github.com>
2024-11-16 23:37:56 +0300

    add get image

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, jsonify
+import os
+
+from flask import Flask, jsonify, send_from_directory
 from gevent.pywsgi import WSGIServer
 import host
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'images'
 
 
 @app.route('/callback', methods=['GET', 'POST'])
@@ -13,7 +16,11 @@ def payment_callback():
 def payment_finish():
     return jsonify({"response_code": "OK"}), 200
 
+@app.route('/images/<filename>', methods=['GET'])
+def get_file_url(filename):
+    full_path = os.path.join(app.root_path, app.config['UPLOAD_FOLDER'])
+    return send_from_directory(full_path, filename)
 
 if __name__ == '__main__':
-    server = WSGIServer((host.ip, host.port), app)
+    server = WSGIServer((host.host, host.port), app)
     server.serve_forever()
